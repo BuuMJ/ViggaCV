acf751e2efbc9bc1e2a6235d4486b376029e8099
Nguyen Ngoc Duoc <duoc6694@gmail.com>
2023-03-18 22:38:37 +0700

    repush

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
-      "mongodb+srv://duoc6694:jJw8rmJmvkZzgIna@viggacv.qxmduwf.mongodb.net/ViggaCV"
+      "mongodb+srv://duoc6694:jJw8rmJmvkZzgIna@viggacv.qxmduwf.mongodb.net/ViggaCV" 
     );
     console.log("Truy cập DB thành công!");
   } catch (error) {
