1b1598d84280393320abcd5cad28d4ed28047ac0
BuuMJ <nxt03091999@gmail.com>
2023-04-01 14:20:21 +0700

    function search for companies page

--- src/app/controllers/CompanyProfileController.js
+++ src/app/controllers/CompanyProfileController.js
@@ -90,7 +90,7 @@ class CompanyProfileController {
                 avatar: avatar,
                 background: background,
                 companydesc: companydesc,
-              }).save();
+              });
               res.redirect("/companyprofile");
             }
             if (avatar) {
@@ -108,7 +108,7 @@ class CompanyProfileController {
                 typeofbusiness: typeofbusiness,
                 avatar: avatar,
                 companydesc: companydesc,
-              }).save();
+              });
               res.redirect("/companyprofile");
             }
 
@@ -127,7 +127,7 @@ class CompanyProfileController {
                 typeofbusiness: typeofbusiness,
                 background: background,
                 companydesc: companydesc,
-              }).save();
+              });
               res.redirect("/companyprofile");
             }
             // res.redirect("/companyprofile");
@@ -269,7 +269,7 @@ class CompanyProfileController {
               companyyears: companyyears,
               typeofbusiness: typeofbusiness,
               companydesc: companydesc,
-            }).save();
+            });
             res.redirect("/companyprofile");
           }
         }
