ac2cb3c59e4c77cb4cf0a84ede54479f3eb4e5e5
Nguyen Ngoc Duoc <duoc6694@gmail.com>
2023-05-15 16:43:36 +0700

    add function pagination

--- src/app/controllers/JobController.js
+++ src/app/controllers/JobController.js
@@ -9,25 +9,60 @@ class JobController {
   async job(req, res, next) {
     const user = req.user;
     const job = await JobModel.find({});
-    const count = await JobModel.countDocuments()
+    const count = await JobModel.countDocuments();
     const company = await CompanyModel.find({});
-    res.render("job", {
-      title: "List Job",
-      job: mutipleMongooseToObject(job),
-      company: mutipleMongooseToObject(company),
-      user,
-      count: count,
-    });
+    var page = req.query.page;
+    var PAGE_SIZE = 5;
+    var total = Math.ceil(count / PAGE_SIZE);
+    console.log("đây là tổng số trang cần có = " + total);
+    if (page) {
+      //Get page
+      page = parseInt(page);
+      var skip = (page - 1) * PAGE_SIZE;
+
+      JobModel.find({})
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .then((data) => {
+          res.render("job", {
+            title: "List Job",
+            job: mutipleMongooseToObject(data),
+            company: mutipleMongooseToObject(company),
+            user,
+            count: count,
+            total: total,
+          });
+        });
+    } else {
+      //Get page
+      page = 1;
+      var skip = (page - 1) * PAGE_SIZE;
+
+      JobModel.find({})
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .then((data) => {
+          res.render("job", {
+            title: "List Job",
+            job: mutipleMongooseToObject(data),
+            company: mutipleMongooseToObject(company),
+            user,
+            count: count,
+            total: total,
+          });
+        });
+    }
   }
 
   //[GET] Job Detail
   async detail(req, res, next) {
-    try {const user = req.user;
+    try {
+      const user = req.user;
       const idjob = req.params.id;
       const detail = await JobModel.findOne({ _id: idjob });
-      const company = await CompanyModel.findOne({iduser: detail.iduser})
-      const job = await JobModel.find({iduser: detail.iduser})
-      console.log(job + 'asdasdgjahgsjhagsfhagjdhgajsdajgajsg')
+      const company = await CompanyModel.findOne({ iduser: detail.iduser });
+      const job = await JobModel.find({ iduser: detail.iduser });
+      console.log(job + "asdasdgjahgsjhagsfhagjdhgajsdajgajsg");
       res.render("jobdetail", {
         title: "Job Detail",
         detail: staffMongoseToObject(detail),
@@ -35,8 +70,8 @@ class JobController {
         company: staffMongoseToObject(company),
         job: mutipleMongooseToObject(job),
       });
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
   }
 }
