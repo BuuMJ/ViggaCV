a530c404a6b6ff18519d09ae7928cf8f146b3bd9
NguyenXuanTruong <nxt03091999@gmail.com>
2023-05-26 10:12:50 +0700

    pull code

--- src/app/controllers/JobController.js
+++ src/app/controllers/JobController.js
@@ -150,6 +150,146 @@ class JobController {
       console.log(err);
     }
   }
+  async search(req, res, next) {
+    const user = req.user;
+    const search = req.query.search;
+
+    const job = await JobModel.find({
+      $or: [
+        { jobname: { $regex: search, $options: "i" } },
+        { companyname: { $regex: search, $options: "i" } },
+      ],
+    });
+    const count = job.length;
+    const company = await CompanyModel.find({});
+    var page = req.query.page;
+    var PAGE_SIZE = 10;
+    var total = Math.ceil(count / PAGE_SIZE);
+    const pages = [];
+    for (let i = 1; i <= total; i++) {
+      pages.push(i);
+    }
+
+    //tìm số job cao nhất
+    const highest = await JobModel.aggregate([
+      {
+        $group: {
+          _id: "$companyname",
+          job_count: { $sum: 1 },
+          iduser: { $first: "$iduser" },
+        },
+      },
+      {
+        $sort: {
+          job_count: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ]);
+
+    //tìm công ty có số job cao nhất
+    if (highest[0]) {
+      var bestCP = await CompanyModel.findOne({ iduser: highest[0].iduser });
+    } else {
+      var bestCP = null;
+    }
+
+    //tìm số follow cao nhất
+    const mostFL = await CompanyModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          companies: {
+            $push: {
+              _id: "$_id",
+              iduser: "$iduser", // Thêm trường iduser vào đối tượng company
+              companyname: "$companyname",
+              follow: "$follow",
+            },
+          },
+          max_follow: { $max: "$follow" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          companies: {
+            $filter: {
+              input: "$companies",
+              as: "company",
+              cond: { $eq: ["$$company.follow", "$max_follow"] },
+            },
+          },
+        },
+      },
+      {
+        $limit: 1, // Giới hạn kết quả trả về là 1
+      },
+    ]);
+    if (mostFL) {
+      var bestFL = await CompanyModel.findOne({
+        iduser: mostFL[0].companies[0].iduser,
+      });
+    } else {
+      var bestFL = null;
+    }
+
+    if (page) {
+      //Get page
+      page = parseInt(page);
+      var skip = (page - 1) * PAGE_SIZE;
+
+      JobModel.find({
+        $or: [
+          { jobname: { $regex: search, $options: "i" } },
+          { companyname: { $regex: search, $options: "i" } },
+        ],
+      })
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .then((data) => {
+          res.render("job", {
+            user,
+            count: count,
+            total: total,
+            pages: pages,
+            title: "List Job",
+            job: mutipleMongooseToObject(data),
+            bestCP: staffMongoseToObject(bestCP),
+            bestFL: staffMongoseToObject(bestFL),
+            company: mutipleMongooseToObject(company),
+          });
+        });
+    } else {
+      //Get page
+      page = 1;
+      var skip = (page - 1) * PAGE_SIZE;
+
+      JobModel.find({
+        $or: [
+          { jobname: { $regex: search, $options: "i" } },
+          { companyname: { $regex: search, $options: "i" } },
+        ],
+      })
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .then((data) => {
+          res.render("job", {
+            user,
+            count: count,
+            total: total,
+            pages: pages,
+            title: "List Job",
+            job: mutipleMongooseToObject(data),
+            bestCP: staffMongoseToObject(bestCP),
+            bestFL: staffMongoseToObject(bestFL),
+            company: mutipleMongooseToObject(company),
+          });
+        });
+    }
+  }
 }
 
 module.exports = new JobController();
--- src/routes/job.js
+++ src/routes/job.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jobController = require("../app/controllers/JobController");
 
+router.get("/search",jobController.search);
 router.get("/:id", jobController.detail);
 router.get("/", jobController.job);
 
